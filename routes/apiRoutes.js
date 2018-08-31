82d060ce0a9988039a15d6b42b7276771da130ac
Andrew <andrewmd4774@gmail.com>
2018-08-30 21:09:15 -0400

    wrote route for updating user correct/incorrect field & question approved/disapproved field

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,12 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  app.get("/api/users/:username/:password", function(req, req) {
+module.exports = function (app) {
+  app.get("/api/users/:username/:password", function (req, req) {
     db.User.findOne({
       where: {
         username: req.params.username
       }
-    }).then(function(user) {
+    }).then(function (user) {
       if (user.password === req.params.password) {
         var data = {
           username: user.username,
@@ -21,19 +21,19 @@ module.exports = function(app) {
   });
 
   // Create a new user
-  app.post("/api/users", function(req, res) {
+  app.post("/api/users", function (req, res) {
     db.User.findOne({
       where: {
         username: req.body.username
       }
-    }).then(function(result) {
+    }).then(function (result) {
       if (result) {
         res.json(null);
       } else {
         db.User.create({
           username: req.body.username,
           password: req.body.password
-        }).then(function(newUser) {
+        }).then(function (newUser) {
           var data = {
             username: newUser.username,
             wins: newUser.wins,
@@ -45,7 +45,7 @@ module.exports = function(app) {
     });
   });
 
-  app.get("api/game/:user", function(req, res) {
+  app.get("api/game/:user", function (req, res) {
     var approved = [];
     var disapproved = [];
     var question = [];
@@ -62,7 +62,7 @@ module.exports = function(app) {
       where: {
         username: req.params.user
       }
-    }).then(function(data) {
+    }).then(function (data) {
       QuestionId = data.QuestionId;
     });
     db.Questions.findAll({
@@ -71,7 +71,7 @@ module.exports = function(app) {
           $gt: QuestionId
         }
       }
-    }).then(function(data) {
+    }).then(function (data) {
       approved = data.approved;
       disapproved = data.disapproved;
       question = data.question;
@@ -81,38 +81,77 @@ module.exports = function(app) {
       answer4 = data.answer4;
       correctAnswer = data.correctAnswer;
     });
-      var rating = approved / (approved + disapproved);
-      if (rating >= 0.5) {
-        res.render("Game", {
-          question: question[i],
-          answer1: answer1[i],
-          answer2: answer2[i],
-          answer3: answer3[i],
-          answer4: answer4[i],
-          correctAnswer: correctAnswer[i]
+    var rating = approved / (approved + disapproved);
+    if (rating >= 0.5) {
+      res.render("Game", {
+        question: question[i],
+        answer1: answer1[i],
+        answer2: answer2[i],
+        answer3: answer3[i],
+        answer4: answer4[i],
+        correctAnswer: correctAnswer[i]
+      });
+    }
+
+    app.post("/api/game/end/:user/:vote/:correct/:QID", function (req, res) {
+      db.Questions.findOne({
+        attributes: ["approved", "disapproved"],
+        WHERE: {
+          QuestionId: req.params.QID
+        }.then(function (data) {
+          if (req.params.vote) {
+            db.Questions.Update({
+              approved: parseInt(data.approved) + 1
+            })
+          }
+
+          else {
+            db.Questions.Update({
+              disapproved: parseInt(data.disapproved) + 1
+            })
+          }
+        })
+      })
+      db.Users.findOne({
+        attributes: ["Correct", "Incorrect"],
+        WHERE: {
+          username: req.params.user
+        }
+      }).then(function (data) {
+        if (req.params.correct) {
+          db.Users.Update({
+            correct: parseInt(data.correct) + 1
+          })
+        }
+
+        else {
+          db.Users.Update({
+            incorrect: parseInt(data.incorrect) + 1
+          })
+        }
+      })
+    })
+
+    // Get all examples
+    app.get("/api/examples", function (req, res) {
+      db.Example.findAll({}).then(function (dbExamples) {
+        res.json(dbExamples);
+      });
+    }),
+      // Create a new example
+      app.post("/api/examples", function (req, res) {
+        db.Example.create(req.body).then(function (dbExample) {
+          res.json(dbExample);
         });
-      };
+      }),
 
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  }),
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  }),
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.json(dbExample);
-    });
+      // Delete an example by id
+      app.delete("/api/examples/:id", function (req, res) {
+        db.Example.destroy({ where: { id: req.params.id } }).then(function (
+          dbExample
+        ) {
+          res.json(dbExample);
+        });
+      })
   })
-})}
+}
